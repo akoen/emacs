419061c2364d808863819a6b4e7c2429bcc58a6e
Alex Koen <arw.koen@gmail.com>
2019-05-10 11:58:03 -0700

    Added doom-mode and consequently all-the-icons

--- custom.el
+++ custom.el
@@ -9,9 +9,6 @@
     ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
  '(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
  '(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")))
- '(org-agenda-files
-   (quote
-    ("~/Org/GCal/gcal-events.org" "~/Org/GCal/gcal-school.org" "/home/alex/Org/cil.org" "/home/alex/Org/cyc.org" "/home/alex/Org/hikes-runs.org" "/home/alex/Org/inbox.org" "/home/alex/Org/manifesto.org" "/home/alex/Org/refile.org" "/home/alex/Org/school.org")))
  '(org-file-apps
    (quote
     ((auto-mode . emacs)
@@ -20,7 +17,7 @@
      ("\\.pdf\\'" . "zathura %s"))))
  '(package-selected-packages
    (quote
-    (helm-themes helm zenburn-theme yasnippet writeroom-mode which-key use-package tao-theme smooth-scrolling ranger poet-theme ox-tufte ox-pandoc org-plus-contrib org-gcal org-download org-bullets olivetti ob-ipython nlinum-relative neotree lsp-ui kaolin-themes habitica gnuplot general focus flycheck-grammalecte flx flatui-theme evil-org evil-magit evil-leader doom-themes deft counsel company-lsp color-identifiers-mode auctex atom-one-dark-theme)))
+    (doom-modeline helm-themes helm zenburn-theme yasnippet writeroom-mode which-key use-package tao-theme smooth-scrolling ranger poet-theme ox-tufte ox-pandoc org-plus-contrib org-gcal org-download org-bullets olivetti ob-ipython nlinum-relative neotree lsp-ui kaolin-themes habitica gnuplot general focus flycheck-grammalecte flx flatui-theme evil-org evil-magit evil-leader doom-themes deft counsel company-lsp color-identifiers-mode auctex atom-one-dark-theme)))
  '(sml/active-background-color "#34495e")
  '(sml/active-foreground-color "#ffffff")
  '(sml/inactive-background-color "#dfe4ea")
