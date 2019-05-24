8ab84adeda9501e31b8808be487ff5f58b7af764
Alex Koen <arw.koen@gmail.com>
2019-05-23 18:12:18 -0700

    "Moved org files to Google Drive"

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(custom-enabled-themes (quote (doom-one)))
  '(custom-safe-themes
    (quote
-    ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
+    ("cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
  '(exec-path
    (quote
     ("/usr/local/sbin" "/usr/local/bin" "/usr/bin" "/usr/lib/emacs/26.2/x86_64-pc-linux-gnu" "/usr/bin/vendor_perl")))
@@ -22,7 +22,7 @@
      ("\\.pdf\\'" . "zathura %s"))))
  '(package-selected-packages
    (quote
-    (ivy-rich toc-org bug-hunter doom-modeline helm-themes helm zenburn-theme yasnippet writeroom-mode which-key use-package tao-theme smooth-scrolling ranger poet-theme ox-tufte ox-pandoc org-plus-contrib org-gcal org-download org-bullets olivetti ob-ipython nlinum-relative neotree lsp-ui kaolin-themes habitica gnuplot general focus flycheck-grammalecte flx flatui-theme evil-org evil-magit evil-leader doom-themes deft counsel company-lsp color-identifiers-mode auctex atom-one-dark-theme)))
+    (load-theme-buffer-local color-theme-buffer-local ivy-rich toc-org bug-hunter doom-modeline helm-themes helm zenburn-theme yasnippet writeroom-mode which-key use-package tao-theme smooth-scrolling ranger poet-theme ox-tufte ox-pandoc org-plus-contrib org-gcal org-download org-bullets olivetti ob-ipython nlinum-relative neotree lsp-ui kaolin-themes habitica gnuplot general focus flycheck-grammalecte flx flatui-theme evil-org evil-magit evil-leader doom-themes deft counsel company-lsp color-identifiers-mode auctex atom-one-dark-theme)))
  '(sml/active-background-color "#34495e")
  '(sml/active-foreground-color "#ffffff")
  '(sml/inactive-background-color "#dfe4ea")
