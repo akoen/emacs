5e42a240fa1ddd8a9f81f0fde6ef6aef9c8c77b5
Alex Koen <arw.koen@gmail.com>
2019-06-19 23:54:44 -0700

    Added counsel-projectile

--- custom.el
+++ custom.el
@@ -5,32 +5,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (doom-one)))
  '(custom-safe-themes
    (quote
-    ("49ec957b508c7d64708b40b0273697a84d3fee4f15dd9fc4a9588016adee3dad" "cd736a63aa586be066d5a1f0e51179239fe70e16a9f18991f6f5d99732cabb32" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
- '(exec-path
-   (quote
-    ("/usr/local/sbin" "/usr/local/bin" "/usr/bin" "/usr/lib/emacs/26.2/x86_64-pc-linux-gnu" "/usr/bin/vendor_perl")))
- '(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
- '(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")))
- '(org-file-apps
-   (quote
-    ((auto-mode . emacs)
-     ("\\.mm\\'" . default)
-     ("\\.x?html?\\'" . default)
-     ("\\.pdf\\'" . "zathura %s"))))
+    ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
  '(package-selected-packages
    (quote
-    (projectile org-pomodoro evil-surround org-protocol load-theme-buffer-local color-theme-buffer-local ivy-rich toc-org bug-hunter doom-modeline helm-themes helm zenburn-theme yasnippet writeroom-mode which-key use-package tao-theme smooth-scrolling ranger poet-theme ox-tufte ox-pandoc org-plus-contrib org-gcal org-download org-bullets olivetti ob-ipython nlinum-relative neotree lsp-ui kaolin-themes habitica gnuplot general focus flycheck-grammalecte flx flatui-theme evil-org evil-magit evil-leader doom-themes deft counsel company-lsp color-identifiers-mode auctex atom-one-dark-theme)))
- '(sml/active-background-color "#34495e")
- '(sml/active-foreground-color "#ffffff")
- '(sml/inactive-background-color "#dfe4ea")
- '(sml/inactive-foreground-color "#34495e"))
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+    (counsel-projectile yasnippet which-key web-mode use-package toc-org smooth-scrolling projectile ox-tufte ox-pandoc org-pomodoro org-plus-contrib org-gcal org-download org-bullets olivetti ob-ipython neotree lsp-ui load-theme-buffer-local ivy-rich habitica gulp-task-runner gnuplot general focus flycheck-grammalecte flx exec-path-from-shell evil-surround evil-org evil-magit doom-themes doom-modeline deft counsel company-lsp auctex)))
+ '(recentf-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
