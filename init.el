3e332c04d42fcf8b0790895f9fec373582bb3e69
akoen <arw.koen@gmail.com>
2020-04-04 07:52:10 -0700

    Improve directory management

--- init.el
+++ init.el
@@ -1,9 +1,26 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+;; Define packages
+(defconst emacs-dir (file-truename user-emacs-directory)
+  "The currently loaded .emacs.d directory.")
+
+(defconst emacs-local-dir (concat emacs-dir ".local/")
+  "Root directory for local storage. Do not share across systems.")
+
+(defconst emacs-cache-dir (concat emacs-local-dir "cache/")
+  "Directory for volatile local storage.")
+
+(defconst emacs-etc-dir (concat emacs-local-dir "etc/")
+  "Directory for non-volatile local storage")
+
+(defconst emacs-packages-dir (concat emacs-local-dir "packages/")
+  "Where pacakges are stored.")
+
 ;; Package management
+(setq straight-base-dir emacs-local-dir)
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" straight-base-dir))
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
