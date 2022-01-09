2ba886d100d455db15251e7f2692b6089161abc5
akoen <arw.koen@gmail.com>
2022-01-08 23:16:49 -0800

    Clean up init.el

--- init.el
+++ init.el
@@ -1,9 +1,11 @@
 ;;; init.el -*- lexical-binding: t; -*-
 (add-to-list 'load-path (concat user-emacs-directory "lisp"))
 
-(if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
-    (load-file (expand-file-name "config.el" user-emacs-directory))
+(setq config-file (expand-file-name "config.el" emacs-dir)
+      custom-file (expand-file-name "custom.el" emacs-dir))
+
+(if (file-exists-p config-file)
+    (load config-file)
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
 
-(setq custom-file (concat emacs-dir "custom.el"))
-(load custom-file)
+(load custom-file t t)
